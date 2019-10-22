085b7ace2bb238fe9dd24d2f0528fe3a38b2b57d
Mikey Epps <mikey@Mikeys-MacBook-Pro.local>
2019-10-22 13:07:48 -0700

    Tweaked reponsive design/JS

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ button.addEventListener('click', readMore)
 function readMore(){
     info.style.display = 'block'
 
+if(screen.width <=768){
    window.scrollBy({
-    top: 1000,
+    top: 812,
     left: 0,
     behavior: 'smooth'
-});
-}
+})}else{
+    window.scrollBy({
+        top: 1000,
+        left: 0,
+        behavior: 'smooth'
+})};
+
+};
+
 
 var info = document.querySelector('#info')
 
